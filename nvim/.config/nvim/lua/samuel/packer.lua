8217bc9a1d8a06e0122d4b9ad67948457668d518
samgj18 <samgj_18@hotmail.com>
2023-04-17 10:34:34 +0100

    Adding Comment.vim for easier commenting files

--- nvim/.config/nvim/lua/samuel/packer.lua
+++ nvim/.config/nvim/lua/samuel/packer.lua
@@ -15,6 +15,7 @@ return require('packer').startup(function(use)
     use { "nvim-lualine/lualine.nvim",
         requires = { "nvim-tree/nvim-web-devicons", opt = true }
     }
+    use { "numToStr/Comment.nvim" }
     use {
         "hrsh7th/nvim-cmp",
         requires = {
