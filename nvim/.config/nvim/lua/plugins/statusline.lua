7096a1e05d18a0779c246ead3c7e7da7b1770220
Samuel Gómez <samgj_18@hotmail.com>
2023-07-20 18:35:48 +0100

    Fixing lazy health

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("lualine").setup {
       options = {
