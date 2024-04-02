0bcc2287cf9891209d79a98588f9d40bd6496239
Samuel Gomez <samgj_18@hotmail.com>
2024-04-02 01:24:41 +0100

    Updating to brand new configuration on Mac

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,20 +3,26 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       playground = { enable = true },
-      auto_install = true,
       query_linter = {
         enable = true,
         use_virtual_text = true,
         lint_events = { "BufWrite", "CursorHold" },
       },
       ensure_installed = "all",
-      highlight = {
-        enable = true,
-        --disable = { "scala" },
+
+      treesitter = {
+        enable = false
       },
-      sync_install = true,
-      modules = {},
+      highlight = {
+        enable = true
+      },
+      indent = {
+        enable = true
+      },
+      auto_install = false,
+      sync_install = false,
       ignore_install = {},
+      modules = {}
     })
   end
 }
