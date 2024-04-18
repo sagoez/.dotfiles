4778a2e8a2a3df87e9b6a4b6598f991a020b8171
Samuel Gomez <samgj_18@hotmail.com>
2024-04-18 09:41:37 +0100

    Adding MD preview plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -38,4 +38,10 @@ return {
   { "wakatime/vim-wakatime" },
   { "windwp/nvim-autopairs" },
   { "github/copilot.vim" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
