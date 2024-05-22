402507290537a9418ff6514e9aa22f05c9828fa6
Samuel Gomez <samgj_18@hotmail.com>
2024-05-22 10:03:14 +0100

    Moving notifications to the bottom of the screen

--- nvim/.config/nvim/lua/plugins/notify.lua
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -1,3 +1,27 @@
 return {
   "rcarriga/nvim-notify",
+  config = function()
+    local notify = require("notify")._config()
+    require("notify").setup({
+      background_colour = "NotifyBackground",
+      fps = 60,
+      icons = {
+        DEBUG = "",
+        ERROR = "",
+        INFO = "",
+        TRACE = "✎",
+        WARN = ""
+      },
+      level = 2,
+      minimum_width = 50,
+      render = "default",
+      stages = "fade_in_slide_out",
+      time_formats = {
+        notification = "%T",
+        notification_history = "%FT%T"
+      },
+      timeout = 3000,
+      top_down = false,
+    })
+  end
 }
