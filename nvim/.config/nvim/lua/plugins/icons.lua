a616a5a39e1128026c5dda926c11246c988b4992
Samuel Gomez <samgj_18@hotmail.com>
2024-09-09 10:38:26 +0100

    Removing gleam icon

--- nvim/.config/nvim/lua/plugins/icons.lua
+++ nvim/.config/nvim/lua/plugins/icons.lua
@@ -3,14 +3,6 @@ return {
   config = function()
     require("nvim-web-devicons").setup({
       default = true,
-      override = {
-        gleam = {
-          icon = "",
-          color = "#ff75a0",
-          cterm_color = "81",
-          name = "Gleam",
-        }
-      }
     });
   end,
   lazy = true,
