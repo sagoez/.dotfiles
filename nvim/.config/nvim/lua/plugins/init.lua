549b93b73610ecc2b7b5442e90e5693eedf2ad0a
Samuel Gomez <samgj_18@hotmail.com>
2024-10-09 20:22:22 +0100

    Replace current work

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -43,6 +43,7 @@ return {
     opts = { open_cmd = "noswapfile vnew" },
     keys = {
       { "<leader>fra", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+      { "<leader>frr", function() require("spectre").open_file_search({ select_word = true }) end, desc = "Replace in current word" },
     },
   },
   {
