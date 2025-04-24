a4e1928e6d29f6eb5ca930623d753637332c9c4d
Samuel Gomez <samgj_18@hotmail.com>
2025-04-24 12:11:35 +0100

    fix: reduce font size

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,7 +30,7 @@ if vim.g.neovide then
   vim.g.neovide_padding_right = 0
   vim.g.neovide_padding_left = 0
   --
-  vim.opt.guifont = "FiraCode Nerd Font:h20"
+  vim.opt.guifont = "FiraCode Nerd Font:h10"
 
 
   vim.keymap.set(
