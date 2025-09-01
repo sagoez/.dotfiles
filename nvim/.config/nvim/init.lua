425bad95f6c390e8aa6997a704e2f251cdc0b173
Samuel Gomez <samgj_18@hotmail.com>
2025-09-01 22:29:10 +0100

    chore: install catpuccin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -39,4 +39,6 @@ if vim.g.neovide then
     function() vim.api.nvim_paste(vim.fn.getreg('+'), true, -1) end,
     { noremap = true, silent = true }
   )
+
+  vim.cmd.colorscheme "catppuccin"
 end
--- nvim/.config/nvim/lua/plugins/ntheme.lua
+++ nvim/.config/nvim/lua/plugins/ntheme.lua
@@ -1,11 +1,13 @@
 return {
-  "navarasu/onedark.nvim",
+  "catppuccin/nvim",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require('onedark').setup {
-      style = 'warm'
-    }
-    -- Enable theme
-    require('onedark').load()
+    require("catppuccin").setup({
+      flavour = "frappe",
+      float = {
+        transparent = false, -- enable transparent floating windows
+        solid = false,       -- use solid styling for floating windows, see |winborder|
+      },
+    })
   end
 }
