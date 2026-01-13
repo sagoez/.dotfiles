1cc8ca8da1ee428384c594875628397e73b0f961
Samuel Gomez <samgj_18@hotmail.com>
2026-01-13 13:03:35 +0000

    fix: disable sharp to fix build

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig, passthroughImageService } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Dotfiles',
-			social: {
-				github: 'https://github.com/sagoez/.dotfiles',
-			},
-			sidebar: [
-				{
-					label: 'Get Started',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Install on Mac', slug: 'guides/install-on-mac' },
-            { label: 'Install on Linux', slug: 'guides/install-on-linux' },
-            { label: 'Folder Structure', slug: 'guides/folder-structure' },
-					],
-				}
-			],
-		}),
-	],
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [
+    starlight({
+      title: "Dotfiles",
+      social: {
+        github: "https://github.com/sagoez/.dotfiles",
+      },
+      sidebar: [
+        {
+          label: "Get Started",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Install on Mac", slug: "guides/install-on-mac" },
+            { label: "Install on Linux", slug: "guides/install-on-linux" },
+            { label: "Folder Structure", slug: "guides/folder-structure" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
